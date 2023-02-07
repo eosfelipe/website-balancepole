993395e4caf48665f2f3409b62f27e82559f8f73
eosfelipe <escobedo.felipe@hotmail.com>
2023-02-07 12:12:34 -0600

    fix assets

--- src/components/react/ModalButton.jsx
+++ src/components/react/ModalButton.jsx
@@ -8,7 +8,7 @@ export default function ModalButton() {
       className="btn btn-primary w-full"
       onClick={() => isModalOpen.set(!$isModalOpen)}
     >
-      Nuestras instalaciones
+      Recorrido virtual
     </button>
   );
 }
